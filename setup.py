1d1039fbc051a65b8ee307d4ef6ec2982eef6d3a
Martin Aspeli <optilude@gmail.com>
2011-10-04 00:29:26 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+version = '1.0b1'
+
+setup(name='plone.resourceeditor',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://svn.plone.org/svn/plone/plone.resource',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'zope.component',
+          'zope.publisher',
+          'zope.schema',
+          'plone.resource',
+          'Zope2',
+      ],
+      extras_require = {
+          'test': ['plone.app.testing']
+      },
+      entry_points="""
+      """,
+      )
