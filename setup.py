802430f6e3a47006b86d0479171a701d51b10325
Maurits van Rees <maurits@vanrees.org>
2018-01-16 12:56:55 +0100

    Reindented setup.py and completed the info in there.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,36 +2,41 @@ from setuptools import setup, find_packages
 
 version = '1.0.1.dev0'
 
-setup(name='plone.resourceeditor',
-      version=version,
-      description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
+setup(
+    name='plone.resourceeditor',
+    version=version,
+    description="Plone resource editor",
+    long_description=open("README.rst").read() + "\n" +
+                   open("CHANGES.rst").read(),
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='https://github.com/plone/plone.resourceeditor',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.component',
-          'zope.publisher',
-          'zope.schema',
-          'plone.resource',
-          'Zope2',
-      ],
-      extras_require = {
-          'test': ['plone.app.testing']
-      },
-      entry_points="""
-      """,
-      )
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='resource editor',
+    author='Martin Aspeli, Plone Foundation',
+    author_email='optilude@gmail.com',
+    url='https://github.com/plone/plone.resourceeditor',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'zope.publisher',
+        'zope.schema',
+        'plone.resource',
+        'Zope2',
+    ],
+    extras_require = {
+        'test': ['plone.app.testing']
+    },
+    entry_points="""
+    """,
+)
